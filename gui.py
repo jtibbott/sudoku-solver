c518ff496f8b5822c3ccdfa6c3e81b2b19e7f1b6
fluxtt <james.tibbott@hotmail.co.uk>
2024-07-28 14:44:19 +0100

    gui to class and init function

--- gui.py
+++ gui.py
@@ -1,12 +1,21 @@
 import tkinter as tk
+from tkinter import messagebox
+from copy import deepcopy
+from SudokuSolver import solve_sudoku
 
-WINDOW_WIDTH = 400
-WINDOW_HEIGHT = 600
-
-# tkinter setup
-root = tk.Tk()
-root.title("Sudoku Solver")
-root.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
-root.resizable(False, False)
-#root.iconbitmap("./assets/sudoku.ico")
-root.mainloop()
+class SudokuGUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Sudoku Solver")
+        self.root.geometry("450x500")
+        self.root.resizable(False, False)
+        
+        self.board = [[0] * 9 for _ in range(9)]
+        self.initial_board = deepcopy(self.board)
+        self.undo_stack = []
+        
+  
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SudokuGUI(root)
+    root.mainloop()
